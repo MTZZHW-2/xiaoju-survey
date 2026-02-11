1acbe15444bb8644189a54b1a143ad0d1fd1deef
MTZZHW <146153789+MTZZHW-2@users.noreply.github.com>
2026-02-11 17:01:15 +0000

    chore: 移除 SDK 嵌入模式预览

--- web/src/management/pages/edit/modules/contentModule/PreviewPanel.vue
+++ web/src/management/pages/edit/modules/contentModule/PreviewPanel.vue
@@ -19,18 +19,12 @@
           >
             <i-ep-monitor />
           </div>
-          <div
-            :class="`preview-tab-item ${previewTab == 3 ? 'active' : ''}`"
-            @click="previewTab = 3"
-          >
-            <i class="iconfont icon-icon_qianrushiwenjuan"></i>
-          </div>
         </div>
         <div
-          :class="`preview-panel ${previewTab == 1 ? 'phone' : previewTab == 2 ? 'pc' : 'sdk'}`"
+          :class="`preview-panel ${previewTab == 1 ? 'phone' : 'pc'}`"
           @click.stop="() => {}"
         >
-          <div class="wrapper" v-if="previewTab !== 3 ">
+          <div class="wrapper">
             <div class="tips-wrapper">
               <i-ep-WarningFilled /> <span>用户预览模式，数据不保存！</span>
             </div>
@@ -44,14 +38,6 @@
                 height="100%"
               ></iframe>
             </div>
-            
-          </div>
-          <div class="sdk-preview" v-else>
-            <div >
-              <el-image :src="sdkImages[sdkType]" fit="contain"/>
-            </div>
-            
-            <el-button class="sdk-preview-btn" type="primary" @click="changeSdkType" :icon="Switch">切换预览模式</el-button>
           </div>
         </div>
       </div>
@@ -61,22 +47,13 @@
 <script setup>
 import { ref, nextTick } from 'vue'
 import { useRoute } from 'vue-router'
-import { Switch } from '@element-plus/icons-vue'
+
 const route = useRoute()
 
 const dialogTableVisible = ref(false)
 const previewTab = ref(1)
 const surveyId = route.params.id
 const loading = ref(true)
-const sdkType = ref(0)
-const sdkImages = [
-  '/imgs/sdk-1.png',
-  '/imgs/sdk-2.png',
-  '/imgs/sdk-3.png'
-  ]
-const changeSdkType = () => {
-  sdkType.value = (sdkType.value + 1) % 3
-}
 
 const onPreview = () => {
   dialogTableVisible.value = true
@@ -204,33 +181,6 @@ const onClose = () => {
         border-radius: 0px 0px 20px 20px;
       }
     }
-    &.sdk { 
-      display: flex;
-      justify-content: center;
-      box-shadow: 0px 2px 10px -2px rgba(82, 82, 102, 0.2);
-      height: 726px;
-      background: #F6F7F9;
-      .wrapper {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-
-        .tips-wrapper {
-          justify-content: center;
-        }
-
-        .iframe-wrapper {
-          width: 636px;
-          flex: 1;
-          margin-top: 20px;
-          border-radius: 8px 8px 0 0;
-          overflow: hidden;
-        }
-      }
-    }
   }
   .tips-wrapper {
     display: flex;
@@ -244,21 +194,6 @@ const onClose = () => {
 
     span {
       margin-left: 5px;
-    }
-  }
-  .sdk-preview{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .el-image{
-      width: 100%;
-      height: 100%;
-    }
-    .el-button{
-      position: absolute;
-      right: 6px;
-      bottom: 24px;
     }
   }
 }
